5960aa939cfd45e6af44ebf0d513582e31343ac7
Diego Fidalgo <diego.fidalgo2@gmail.com>
2021-09-12 17:42:20 -0300

    Adicionadas mais regras ao ESLint

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,5 +1,7 @@
-// eslint-disable-next-line no-undef
-module.exports = {
+/**
+ * @type {import('eslint').Linter.Config}
+ */
+const config = {
     env: {
         browser: true,
         es2021: true
@@ -12,7 +14,8 @@ module.exports = {
     rules: {
         'indent': [
             'error',
-            4
+            4,
+            { SwitchCase: 1 }
         ],
         'linebreak-style': [
             'error',
@@ -29,5 +32,21 @@ module.exports = {
         ],
         'eqeqeq': 'error',
         'no-var': 'error',
+        'prefer-const': 'error',
+        'no-case-declarations': 'off',
+        'no-unsafe-optional-chaining': [
+            'error',
+            { disallowArithmeticOperators: true }
+        ],
+        'no-unreachable-loop': 'error',
+        'dot-location': [
+            'error',
+            'property'
+        ],
+        'array-callback-return': 'error',
+        'dot-notation': 'error',
     }
 };
+
+// eslint-disable-next-line no-undef
+module.exports = config;
