c082c50a0ed299939f50070be1e73a13b89f38c4
Ana Paula Schuch <aplschuch@gmail.com>
2021-10-04 12:02:43 -0300

    [WIP] Teste camera

--- src/index.js
+++ src/index.js
@@ -13,7 +13,9 @@ document.body.appendChild(canvas);
 const scene = new THREE.Scene();
 const camera = new THREE.PerspectiveCamera(
     75,
-    window.innerWidth / window.innerHeight
+    window.innerWidth / window.innerHeight,
+    0.1,
+    10000
 );
 
 const renderer = new THREE.WebGLRenderer({
@@ -22,19 +24,93 @@ const renderer = new THREE.WebGLRenderer({
     alpha: true
 });
 
-window.addEventListener('resize', () => {
-    camera.aspect = window.innerWidth / window.innerHeight;
-    camera.updateProjectionMatrix();
-    renderer.setSize(imageDimensions.width, imageDimensions.height);
-});
+const axesHelper = new THREE.AxesHelper(50);
+scene.add(axesHelper);
+const gridHelper = new THREE.GridHelper(1, 1);
+scene.add(gridHelper);
 
-const geometry = new THREE.BoxGeometry(1, 1, 1);
+// window.addEventListener('resize', () => {
+//     camera.aspect = window.innerWidth / window.innerHeight;
+//     camera.updateProjectionMatrix();
+//     renderer.setSize(imageDimensions.width, imageDimensions.height);
+// });
+
+const m = new THREE.Matrix4();
+
+const trans = new THREE.Matrix4();
+trans.makeTranslation(-1, 1, 0);
+
+const rX = new THREE.Matrix4();
+rX.makeRotationX((180 * Math.PI) / 180);
+
+// const rY = new THREE.Matrix4();
+// rY.makeRotationY((20 * Math.PI) / 180);
+
+// const rZ = new THREE.Matrix4();
+// rZ.makeRotationZ((46 * Math.PI) / 180);
+
+const esc = new THREE.Matrix4();
+esc.makeScale(2 / 1920, 2 / 1080, 1);
+
+const rot = new THREE.Matrix4();
+rot.makeRotationFromQuaternion(new THREE.Quaternion(0.52, 0.25, 0.241, 0.78));
+
+m.multiply(trans);
+m.multiply(rX);
+//m.multiply(rY);
+//m.multiply(rZ);
+m.multiply(esc);
+//m.multiply(rot);
+
+//m.set(1, 0, 0, -0.7, 0, 1, 0, 0, 0, 0, 1, 0, 0, 0, 0, 1);
+
+const mcam = new THREE.Matrix4();
+mcam.set(
+    1.25739582e2,
+    2.11867494e2,
+    -6.20730557e1,
+    8.44056055e2,
+    -3.30132837,
+    -4.8253344e1,
+    -2.3892796e2,
+    8.72524733e2,
+    -5.76879082e-2,
+    6.22131521e-2,
+    -2.99223647e-2,
+    1.0,
+    0,
+    0,
+    0,
+    1
+);
+m.multiply(mcam);
+
+console.log(m);
+
+camera.matrixAutoUpdate = false;
+camera.projectionMatrix = m;
+//camera.projectionMatrix.elements[4] = -0.67;
+//camera.projectionMatrix.elements[5] = 6.8;
+//camera.projectionMatrix.elements[6] = 0.5;
+
+console.log(camera.projectionMatrix);
+//console.log(camera.projectionMatrix);
+//camera.updateProjectionMatrix();
+const geometry = new THREE.BoxGeometry(20, 20, 0);
 const material = new THREE.MeshLambertMaterial({
-    color: 0x00ffff,
+    color: 0xffffff,
     wireframe: false
 });
 
 const cube = new THREE.Mesh(geometry, material);
+
+const cube2 = new THREE.Mesh(geometry, material);
+cube2.position.set(10, 10, 0);
+scene.add(cube2);
+
+const cube3 = new THREE.Mesh(geometry, material);
+cube3.position.set(1920, 1080, 0);
+scene.add(cube3);
 
 const light = new THREE.DirectionalLight(0xffffff, 1);
 
