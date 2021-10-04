e86e972df92d866a5af332bac4d1660ca6a356c8
Diego Fidalgo <diego.fidalgo2@gmail.com>
2021-10-04 17:04:32 -0300

    Fix: ângulo de inclinação incorreto para diferentes tamanhos de tela

--- src/index.js
+++ src/index.js
@@ -14,13 +14,32 @@ const renderer = new THREE.WebGLRenderer({
     alpha: true
 });
 
-renderer.setSize(window.innerWidth, window.innerHeight);
+const BASE_WIDTH = 1920;
+const BASE_HEIGHT = 948;
+const BASE_ASPECT = BASE_WIDTH / BASE_HEIGHT;
 
-window.addEventListener('resize', () => {
+// Seno do ângulo de inclinação da pintura amarela na fotografia estimado para
+// o aspecto 1920:948.
+const baseSin = Math.sin(THREE.Math.degToRad(4.2));
+
+function setView() {
+    // Definição do tamanho da vista
     renderer.setSize(window.innerWidth, window.innerHeight);
     camera.aspect = window.innerWidth / window.innerHeight;
+    // Ajustes na posição da câmera
+    camera.position.y = 3.5;
+    camera.position.z = -5;
+    // Correção na imperfeição do centro da fotografia
+    camera.position.x = 0.18 * window.innerWidth / BASE_WIDTH;
+    // Correção da imperfeição da rotação da fotografia
+    camera.rotation.y = Math.PI - Math.asin(baseSin * camera.aspect / BASE_ASPECT);
+    // Atualização da matriz de projeção
     camera.updateProjectionMatrix();
-});
+}
+
+setView();
+
+window.addEventListener('resize', setView);
 
 const geometry = new THREE.BoxGeometry(1, 1, 1);
 const material = new THREE.MeshLambertMaterial({
@@ -33,10 +52,6 @@ const cube = new THREE.Mesh(geometry, material);
 const light = new THREE.DirectionalLight(0xffffff, 1);
 
 scene.add(cube, light);
-camera.position.y = 3.5;
-camera.position.x = 0.1;
-camera.position.z = -5;
-camera.rotateY(THREE.Math.degToRad(176));
 
 if (import.meta.env.DEV) {
     const axes = new THREE.AxesHelper(2);
