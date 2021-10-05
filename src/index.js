4c7b81f6f07345b83adc99f15170e1889e71daf3
Ana Paula Schuch <aplschuch@gmail.com>
2021-10-05 00:23:22 -0300

    Adicionado carrinho

--- src/index.js
+++ src/index.js
@@ -1,11 +1,12 @@
 import * as THREE from 'three';
+import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls';
 
 const canvas = document.querySelector('canvas');
 
 const scene = new THREE.Scene();
 const camera = new THREE.PerspectiveCamera(
     75,
-    window.innerWidth / window.innerHeight,
+    window.innerWidth / window.innerHeight
 );
 
 const renderer = new THREE.WebGLRenderer({
@@ -30,9 +31,10 @@ function setView() {
     camera.position.y = 3.5;
     camera.position.z = -5;
     // Correção na imperfeição do centro da fotografia
-    camera.position.x = 0.18 * window.innerWidth / BASE_WIDTH;
+    camera.position.x = (0.18 * window.innerWidth) / BASE_WIDTH;
     // Correção da imperfeição da rotação da fotografia
-    camera.rotation.y = Math.PI - Math.asin(baseSin * camera.aspect / BASE_ASPECT);
+    camera.rotation.y =
+        Math.PI - Math.asin((baseSin * camera.aspect) / BASE_ASPECT);
     // Correção do ângulo de projeção
     // NÃO há nenhuma garantia de que isso funcione em telas diferentes
     camera.rotation.x = THREE.Math.degToRad(-1.3);
@@ -44,22 +46,76 @@ setView();
 
 window.addEventListener('resize', setView);
 
-const geometry = new THREE.BoxGeometry(1, 1, 1);
+// criação do grupo que será futuramente um carrinho
+const car = new THREE.Group();
 const material = new THREE.MeshLambertMaterial({
     color: 0x00ffff,
     wireframe: false
 });
 
-const cube = new THREE.Mesh(geometry, material);
+// rodas
+const wheelGeometry = new THREE.TorusGeometry(1 / 7, 1 / 6, 16, 100);
+const wheel = new THREE.Mesh(wheelGeometry, material);
+wheel.rotateY((90 * Math.PI) / 180);
+wheel.translateY(1 / 7 + 1 / 6);
+
+const wheelPositions = [
+    [1 - 1 / 3, -0.55 + 1 / 6],
+    [1 - 1 / 3, 0.55 - 1 / 6],
+    [-1 + 1 / 3, -0.55 + 1 / 6],
+    [-1 + 1 / 3, 0.55 - 1 / 6]
+];
+
+for (let i = 0; i < wheelPositions.length; i++) {
+    const m = wheel.clone();
+    m.translateX(wheelPositions[i][0]);
+    m.translateZ(wheelPositions[i][1]);
+    car.add(m);
+}
+
+// parte superior do carro (paralelepípedo)
+const upGeometry = new THREE.BoxGeometry(1, 0.4, 0.6);
+const cubeUp = new THREE.Mesh(upGeometry, material);
+cubeUp.position.y = 1 / 6 + 1 / 7 + 0.8;
+cubeUp.position.z = -0.3;
+car.add(cubeUp);
+
+// parte inferior do carro (paralelepípedo)
+const downGeometry = new THREE.BoxGeometry(1, 0.6, 2);
+const cubeDown = new THREE.Mesh(downGeometry, material);
+cubeDown.position.y = 1 / 6 + 1 / 7 + 0.3;
+car.add(cubeDown);
+
+// estrutura auxiliar (1/4 de cilindro) para o carro
+// ficar com arestas mais arredondadas
+const cilynderGeometry = new THREE.CylinderGeometry(
+    0.4,
+    0.4,
+    1,
+    32,
+    32,
+    false,
+    0,
+    Math.PI / 2
+);
+const auxGeo = new THREE.Mesh(cilynderGeometry, material);
+auxGeo.rotateZ(Math.PI / 2);
+auxGeo.translateX(1 / 6 + 1 / 7 + 0.6);
+car.add(auxGeo);
+
+const auxGeo2 = auxGeo.clone();
+auxGeo2.rotateX(Math.PI);
+auxGeo2.translateZ(0.6);
+car.add(auxGeo2);
 
 const light = new THREE.DirectionalLight(0xffffff, 1);
 
-scene.add(cube, light);
+scene.add(car, light);
 
 if (import.meta.env.DEV) {
     const axes = new THREE.AxesHelper(2);
     const grid = new THREE.GridHelper(9.5, 10);
-
+    //const controls = new OrbitControls(camera, renderer.domElement);
     scene.add(axes, grid);
 }
 
@@ -68,30 +124,34 @@ const movements = {
     z: 0
 };
 
-document.addEventListener('keydown', e => {
+document.addEventListener('keydown', (e) => {
     if (e.repeat) return;
 
-    switch(e.key) {
+    switch (e.key) {
         case 'w':
         case 'W':
         case 'ArrowUp':
-            movements.z = +0.2; break;
+            movements.z = +0.2;
+            break;
         case 's':
         case 'S':
         case 'ArrowDown':
-            movements.z = -0.2; break;
+            movements.z = -0.2;
+            break;
         case 'a':
         case 'A':
         case 'ArrowLeft':
-            movements.x = +0.05; break;
+            movements.x = +0.05;
+            break;
         case 'd':
         case 'D':
         case 'ArrowRight':
-            movements.x = -0.05; break;
+            movements.x = -0.05;
+            break;
     }
 });
 
-document.addEventListener('keyup', e => {
+document.addEventListener('keyup', (e) => {
     if (e.repeat) return;
 
     switch (e.key) {
@@ -101,14 +161,16 @@ document.addEventListener('keyup', e => {
         case 'S':
         case 'ArrowUp':
         case 'ArrowDown':
-            movements.z = 0; break;
+            movements.z = 0;
+            break;
         case 'a':
         case 'A':
         case 'd':
         case 'D':
         case 'ArrowLeft':
         case 'ArrowRight':
-            movements.x = 0; break;
+            movements.x = 0;
+            break;
     }
 });
 
@@ -118,14 +180,13 @@ function animate() {
     requestAnimationFrame(animate);
 
     for (const [dir, inc] of Object.entries(movements)) {
-        cube.position[dir] += inc;
+        car.position[dir] += inc;
     }
 
-    if (cube.position.z > infinity) {
-        cube.position.z = -1;
-    }
-    else if (cube.position.z < -1) {
-        cube.position.z = infinity;
+    if (car.position.z > infinity) {
+        car.position.z = -1;
+    } else if (car.position.z < -1) {
+        car.position.z = infinity;
     }
 
     renderer.render(scene, camera);
